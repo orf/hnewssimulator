fbeb2c79333f75997d0a95b2f9810679acfac0cd
Tom <tom@tomforb.es>
2015-09-20 15:36:05 +0100

    Set all_kids to NULL after importing

--- import.sh
+++ import.sh
@@ -1,2 +1,4 @@
+#!/usr/bin/env bash
 pv data/comments.csv | sudo -u postgres psql hnews -c "COPY comments FROM STDIN DELIMITER ',' CSV"
 pv data/posts.csv | sudo -u postgres psql hnews -c "COPY posts FROM STDIN DELIMITER ',' CSV"
+sudo -u postgres psql hnews -c "UPDATE posts SET all_kids=NULL";
